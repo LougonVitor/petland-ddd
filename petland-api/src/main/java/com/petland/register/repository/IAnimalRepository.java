199b08a93ce948f13f956c726f30f9e4ea54cad3
Vítor Lougon <vitor.lougon@gmail.com>
2025-11-01 11:07:25 -0300

    feat: creating findBy for animal name and specie

--- petland-api/src/main/java/com/petland/register/repository/IAnimalRepository.java
+++ petland-api/src/main/java/com/petland/register/repository/IAnimalRepository.java
@@ -1,9 +1,10 @@
 package com.petland.register.repository;
 
+import com.petland.register.model.AnimalSpecies;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.petland.register.model.entity.AnimalEntity;
 
 public interface IAnimalRepository extends JpaRepository<AnimalEntity, Integer>{
-
+    AnimalEntity findByNameAndSpecie(String name, AnimalSpecies specie);
 }
